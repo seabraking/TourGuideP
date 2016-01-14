51b85561c36b7616a67c2a3f5dd8bb61d5cb14ac
seabraking <pgmseabra@gmail.com>
2016-01-14 17:10:47 +0100

    tu adoras

--- app/src/main/java/pt/ipp/estgf/tourguide/Adapter/SearchableLocalAdapter.java
+++ app/src/main/java/pt/ipp/estgf/tourguide/Adapter/SearchableLocalAdapter.java
@@ -171,7 +171,7 @@ public class SearchableLocalAdapter extends BaseAdapter implements Filterable, L
             if(filterString.startsWith("q:")){
                 Toast.makeText(context, filterString, Toast.LENGTH_SHORT).show();
                 String[] str = filterString.split("q:");
-                filterString = str[0];
+                filterString = str[1];
                 for (int i = 0; i < count; i++) {
                 filterableString = list.get(i);
                 if (filterableString.getNome().toLowerCase().contains(filterString)) {
