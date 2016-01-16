504cb6715009077263a2ad02fb0656092a7e9d44
seabraking <pgmseabra@gmail.com>
2016-01-16 23:24:46 +0100

    tu adoras

--- app/src/main/java/pt/ipp/estgf/tourguide/Widgets/AppWidgetViewsFactory.java
+++ app/src/main/java/pt/ipp/estgf/tourguide/Widgets/AppWidgetViewsFactory.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -24,7 +28,7 @@ public class AppWidgetViewsFactory implements RemoteViewsService.RemoteViewsFact
     private Context context = null;
     //private int appWidgetId;
     private ArrayList<Local> arrayList = new ArrayList<Local>();
-
+    String stringLatitude = "", stringLongitude = "";
     public AppWidgetViewsFactory(Context ctxt, Intent intent) {
         this.context = ctxt;
 		/*appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
@@ -63,23 +67,23 @@ public class AppWidgetViewsFactory implements RemoteViewsService.RemoteViewsFact
         row.setTextViewText(R.id.txtDesc,arrayList.get(position).getDescricao());
 
         //
-        //GPS
         GPSTracker gpsTracker = new GPSTracker(context, this);
-        String stringLatitude = "", stringLongitude = "", nameOfLocation="";
+        checkGPS();
 
         if (gpsTracker.canGetLocation()) {
             stringLatitude = String.valueOf(gpsTracker.getLatitude());
             stringLongitude = String.valueOf(gpsTracker.getLongitude());
+            DecimalFormat df = new DecimalFormat("#.0");
             Double lat1 = (Double.parseDouble(stringLatitude));
             Double long1 = Double.parseDouble(stringLongitude);
             Double lat2 = Double.parseDouble(arrayList.get(position).getCoordenadas().getLatitude());
             Double long2 = Double.parseDouble(arrayList.get(position).getCoordenadas().getLongitude());
-            DecimalFormat df = new DecimalFormat("#.0");
-            row.setTextViewText(R.id.txtDistancia, " (" + df.format(gpsTracker.distance(lat1,long1,lat2, long2)) + " km)");
+            row.setTextViewText(R.id.txtDesc,arrayList.get(position).getDescricao()+ df.format(gpsTracker.distance(lat1,long1,lat2, long2)));
         } else {
-            row.setTextViewText(R.id.txtDistancia, "No Gps");
+            row.setTextViewText(R.id.txtDesc, "No Gps");
 
         }
+
         Intent i = new Intent();
         Bundle extras = new Bundle();
 
@@ -112,21 +116,46 @@ public class AppWidgetViewsFactory implements RemoteViewsService.RemoteViewsFact
 
     @Override
     public void onDataSetChanged() {
+     /*   GestorLocaisInteresse gestor = new GestorLocaisInteresse();
+        arrayList.clear();
+       arrayList.addAll(gestor.listar(context));*/
+     //   arrayList.add(new Local("oi","oi","oi","","","",""));
+
+
+    }
+
+    public void checkGPS(){
+
+        //GPS
+        GPSTracker gpsTracker = new GPSTracker(context, this);
+
+
+        if (gpsTracker.canGetLocation()) {
+            stringLatitude = String.valueOf(gpsTracker.getLatitude());
+            stringLongitude = String.valueOf(gpsTracker.getLongitude());
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+
 
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
+        checkGPS();
+        onDataSetChanged();
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
+        checkGPS();
+        onDataSetChanged();
 
     }
 
     @Override
     public void onProviderEnabled(String provider) {
+        checkGPS();
 
     }
 
